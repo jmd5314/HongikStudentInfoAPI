532f6a812ab628f2d8547c2899d9c838afad734f
jmd5314 <jmd5314@gmail.com>
2023-09-25 15:59:17 +0900

    초기 설정

---
+++ HongikStudentInfoAPI/src/main/java/com/jo/belief/HongikStudentInfoAPI/HongikStudentInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.jo.belief.HongikStudentInfoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikStudentInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikStudentInfoApiApplication.class, args);
+	}
+
+}
